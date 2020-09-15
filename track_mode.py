16347f194452e40ff14f27dbd97bc3cc0c4dbf55
Alex Istrate <calzzon3@gmail.com>
2020-09-15 18:07:50 +0300

    Countdown in tracking mode.

--- track_mode.py
+++ track_mode.py
@@ -33,7 +33,7 @@ from move_1_deg import *
 
 # Countdown in tracking mode.
 def countdown(*args):
-	print(f"Next move in {args[0]} seconds...\n")
+	print("Next move in " + str(args[0]) + " seconds...")
 	if args[0] <= 1: os.kill(args[1], signal.SIGINT)
 
 # in tracking moe,teh user searches for a body, the program goes there and then recomputes its coordonates and moves accordingly every few seconds
@@ -49,7 +49,8 @@ def wait_for(timeout):
 	print(f"Auto-tracking with manual control. Press 'c' to cancel, ? / ! to get status. Next move in {timeout} seconds. ")
 
 	# spawn many delayed prints, one for every second in timeout
-	timers = [Timer(i, countdown, (timeout-i, pid)) for i in range(int(round(timeout, 0)))]
+	#timers = [Timer(i, countdown, (timeout-i, pid)) for i in range(int(round(timeout, 0)))]
+	timers = [Timer(i, lambda: print("Next move in " + str(timeout-i) + " seconds...")) for i in range(int(round(timeout, 0)))]
 	for t in timers: t.start()
 
 
