6a82f326f6352c275e1e7fa435e6b2c995a39c7f
Alex Istrate <calzzon3@gmail.com>
2020-09-15 18:26:26 +0300

    Countdown in tracking mode.

--- track_mode.py
+++ track_mode.py
@@ -33,7 +33,7 @@ from move_1_deg import *
 
 # Countdown in tracking mode.
 def countdown(*args):
-	print("Next move in " + str(args[0]) + " seconds...")
+	print("Next move in " + str(args[0]) + " seconds...", end=" ")
 
 # in tracking moe,teh user searches for a body, the program goes there and then recomputes its coordonates and moves accordingly every few seconds
 # meanwhile, the user can manually fine-tune the position of the telescope so the actual position coresponds to the position the software thinks it should be pointing to
@@ -48,7 +48,7 @@ def wait_for(timeout):
 	print("Auto-tracking with manual control. Press 'c' to cancel, ? / ! to get status. Next move in " + str(timeout) + " seconds. ")
 
 	# spawn many delayed prints, one for every second in timeout
-	timers = [Timer(i, countdown, timeout-i) for i in range(int(round(timeout, 0)))]
+	timers = [Timer(i, countdown, (timeout-i)) for i in range(int(round(timeout, 0)))]
 	#timers = [Timer(i, lambda: print("Next move in " + str(timeout-i) + " seconds...")) for i in range(int(round(timeout, 0)))]
 	for t in timers: t.start()
 
